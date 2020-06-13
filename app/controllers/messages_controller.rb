26a0580c44cfce0c41bd937b4a62b14fc699e659
mayukoala <mayu.3104.j@gmail.com>
2020-06-13 18:05:02 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all
+  end
+
+  def create
+    Message.create(messgae_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
