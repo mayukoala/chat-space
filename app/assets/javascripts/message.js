1cc7683fcdc9ebdc6bb6118f06a0e28016c2923c
mayukoala <mayu.3104.j@gmail.com>
2020-07-02 17:01:34 +0900

    Update mssage.js/Fix jbuilder

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -85,10 +85,10 @@ $(function() {
     })
     .fail(function() {
       alert('error');
-    })
+    });
   }
-  if (document.location.href.match(/\/groups\/d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
-  }
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+    }
   
 });
